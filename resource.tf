e942c60546411b9719eff556ff17ed4e29d097e9
lynchtsean <lynchtsean@gmail.com>
2025-05-02 19:20:03 -0400

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,7 +1,6 @@
 resource
 "azurerm_resource_group"
 "lynchterraform"{
-  name  =
-  "mcit_resource_group_lynchterraform"
+  name  ="mcit_resource_group_lynchterraform"
   location="canadacentral"
   }
