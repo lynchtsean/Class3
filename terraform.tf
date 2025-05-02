9fe59294d5c4ba31c30361aa01bd83d9982cc52b
lynchtsean <lynchtsean@gmail.com>
2025-05-02 19:02:08 -0400

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,9 @@
+terraform{
+  required_providers{
+    azurerm={
+      source="hashicorp/asurerm"
+       version=">=3.70.0"#this version is for azurerm, NOT terraform version
+  }
+}
+required_version="1.4.0"#this version is for Terraform Version, NOT azurerm
+}
