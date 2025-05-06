3d6f9c39b4de7367c3477d684dea6c560c95c861
lynchtsean <lynchtsean@gmail.com>
2025-05-05 20:13:28 -0400

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -2,8 +2,8 @@ resource "azurerm_storage_account" "mcitstorage" {
   name                      = var.lynchterraform
   resource_group_name       = azurerm_resource_group.lynchterraform.name
   location                  = azurerm_resource_group.lynchterraform.location
-  account_tier              = "Standard"
-  account_replication_type  = "LRS"  
+  account_tier              = "Premium"
+  account_replication_type  = "ZRS"  
   
   tags = {
     environment = "dev"
