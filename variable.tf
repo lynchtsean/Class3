c8535ec2adb58b3015a550efae8ca425c66081e8
lynchtsean <lynchtsean@gmail.com>
2025-05-05 20:19:00 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -10,6 +10,14 @@ variable "client_secret"{
 variable "tenant_id"{
   type=string
 }
+variable "account_tier"{
+  type=string
+  default="Premium"
+}
+variable "account_replication_type"{
+  type=string
+  default="ZRS"
+}
 variable "lynchterraform"{
   type=string
   default="lynchterraform"
