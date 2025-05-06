281ddf12679b12a080768dda02f4462f75a6985a
lynchtsean <lynchtsean@gmail.com>
2025-05-05 20:46:47 -0400

    Update storagecontainer.tf

--- storagecontainer.tf
+++ storagecontainer.tf
@@ -1,4 +1,4 @@
-resource "azurerm_storage_container" "mcitstorage" {
+resource "azurerm_storage_container" "lynchterraform" {
   name                  = "content"
   storage_account_id    = azurerm_storage_account.mcitstorage.id
   container_access_type = "private"
