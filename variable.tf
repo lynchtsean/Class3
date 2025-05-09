9cfdbe30453a0747fd0f6da9d69526fb97734784
lynchtsean <lynchtsean@gmail.com>
2025-05-09 18:15:08 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -23,6 +23,6 @@ variable "lynchterraform"{
   default="lynchterraform"
 }
 variable "string_numbers"{
-  type+list
+  type=list
   default=["42", "77", "13", "98", "3", "64", "55", "27", "89", "6"]
 }
