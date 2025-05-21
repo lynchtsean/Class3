9cf67c3478c306e7b60aba3268d71828770c9c41
lynchtsean <lynchtsean@gmail.com>
2025-05-21 19:59:43 -0400

    Update mcityaml.tf

--- mcityaml.tf
+++ mcityaml.tf
@@ -15,8 +15,8 @@ locals {
 resource "azurerm_service_plan" "grinch24op" {
   for_each            ={for sp in local.windows_app_list: "${sp.name}"=>sp }
   name                = each.value.name
-  resource_group_name = azurerm_resource_group.mcitdevrm.name
-  location            = azurerm_resource_group.mcitdevrm.location
+  resource_group_name = azurerm_resource_group.lynchterraform.name
+  location            = azurerm_resource_group.lynchterraform.location
   os_type             = each.value.os_type
   sku_name            = each.value.sku_name
 }
@@ -24,8 +24,8 @@ resource "azurerm_service_plan" "grinch24op" {
 resource "azurerm_windows_web_app" "grinch24op" {
   for_each            = azurerm_service_plan.mcitdevrm
   name                = each.value.name
-  resource_group_name = azurerm_resource_group.mcitdevrm.name
-  location            = azurerm_resource_group.mcitdevrm.location
+  resource_group_name = azurerm_resource_group.lynchterraform.name
+  location            = azurerm_resource_group.lynchterraform.location
   service_plan_id     = each.value.id
 
   site_config {}
