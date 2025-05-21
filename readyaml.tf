08226869dea96d85e9da6f23b9d9347875839090
lynchtsean <lynchtsean@gmail.com>
2025-05-21 18:55:53 -0400

    Update readyaml.tf

--- readyaml.tf
+++ readyaml.tf
@@ -1,5 +1,5 @@
 #test
-locals{
+locals {
   linux_app=[for f in fileset("${path.module}/configs", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/${f}"))]
   linux_app_list = flatten([
     for app in local.linux_app : [
