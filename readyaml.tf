312901a0453f096fe352d0e5bbb463ce01208a86
lynchtsean <lynchtsean@gmail.com>
2025-05-21 19:00:45 -0400

    Update readyaml.tf

--- readyaml.tf
+++ readyaml.tf
@@ -15,7 +15,7 @@ locals {
 resource "azurerm_service_plan" "batcha06sp" {
   for_each            ={for sp in local.linux_app_list: "${sp.name}"=>sp }
   name                = each.value.name
-  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  resource_group_name = azurerm_resource_group.azureresourcegroup."lynchterraform"
   location            = azurerm_resource_group.azureresourcegroup.location
   os_type             = each.value.os_type
   sku_name            = each.value.sku_name
