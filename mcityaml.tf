ec3c38453748d5c7ddbe816a1f64195a56f18dc3
lynchtsean <lynchtsean@gmail.com>
2025-05-21 20:22:12 -0400

    Update mcityaml.tf

--- mcityaml.tf
+++ mcityaml.tf
@@ -1,6 +1,7 @@
 #test
-locals {
-  windows_app=[for f in fileset("${path.module}/mcityaml", "[^_]*.yaml") : yamldecode(file("${path.module}/mcityaml/${f}"))]
+locals{
+  folderlocation="mcityaml"
+  windows_app=[for f in fileset("${path.module}/${local.folderlocation}", "[^_]*.yaml") : yamldecode(file("${path.module}/${local.folderlocation}/${file}"))]
   windows_app_list = flatten([
     for app in local.windows_app : [
       for winapps in try(app.listofwindowsapp, []) :{
@@ -11,21 +12,25 @@ locals {
     ]
 ])
 }
-   
-resource "azurerm_service_plan" "grinch24op" {
+
+resource "azurerm_resource_group" "mcitdevrm" {
+  name     = "mcitwindowsapp"
+  location = "canadacentral"
+}
+resource "azurerm_service_plan" "grinch24opserviceplan" {
   for_each            ={for sp in local.windows_app_list: "${sp.name}"=>sp }
   name                = each.value.name
-  resource_group_name = azurerm_resource_group.lynchterraform.name
-  location            = azurerm_resource_group.lynchterraform.location
+  resource_group_name = azurerm_resource_group.mcitdevrm.name
+  location            = azurerm_resource_group.mcitdevrm.location
   os_type             = each.value.os_type
   sku_name            = each.value.sku_name
 }
 
-resource "azurerm_windows_web_app" "grinch24op" {
-  for_each            = azurerm_service_plan.grinch24op
+resource "azurerm_windows_web_app" "grinch24opwebapp" {
+  for_each            = azurerm_service_plan.grinch24opserviceplan
   name                = each.value.name
-  resource_group_name = azurerm_resource_group.lynchterraform.name
-  location            = azurerm_resource_group.lynchterraform.location
+  resource_group_name = azurerm_resource_group.mcitdevrm.name
+  location            = azurerm_resource_group.mcitdevrm.location
   service_plan_id     = each.value.id
 
   site_config {}
