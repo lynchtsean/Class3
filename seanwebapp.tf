f6a14b3390d6aa27f6d2995b9203378cfd343888
lynchtsean <lynchtsean@gmail.com>
2025-05-23 19:29:25 -0400

    Update seanwebapp.tf

--- seanwebapp.tf
+++ seanwebapp.tf
@@ -1,6 +1,6 @@
-resource "azurerm_linux_web_app" "example" {
+resource "azurerm_linux_web_app" "sean" {
   name                = var.linux_web_app_name
   resource_group_name = var.resource_group_name
   location            = var.location
-  service_plan_id     = azurerm_service_plan.example.id
+  service_plan_id     = azurerm_service_plan.sean.id
 }
