3ae0c353bf402732069af57147d6bdd63ef1b680
lynchtsean <lynchtsean@gmail.com>
2025-05-23 19:51:00 -0400

    Update seanwebapp.tf

--- seanwebapp.tf
+++ seanwebapp.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "mynamegroup" {
   location = "canadacentral"
 }
 resource "azurerm_service_plan" "seanserviceplan" {
-  name                = "${var.seanwebapp}sp"
+  name                = "${var.myname}sp"
   resource_group_name = azurerm_resource_group.mynamegroup.name
   location            = azurerm_resource_group.mynamegroup.location
   os_type             = "p1v2"
