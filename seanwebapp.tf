87ff114d3bc60cc60d925ab646aaafe732de98e5
lynchtsean <lynchtsean@gmail.com>
2025-05-23 20:00:05 -0400

    Update seanwebapp.tf

--- seanwebapp.tf
+++ seanwebapp.tf
@@ -1,19 +1,19 @@
 resource "azurerm_resource_group" "seanrg" {
-  name     = "${var.seanwebapp}rm"
+  name     = "${var.my_name}rg"
   location = "canadacentral"
 }
 resource "azurerm_service_plan" "seanserviceplan" {
-  name                = "${var.seanwebapp}sp"
-  resource_group_name = azurerm_resource_group.mynamegroup.name
-  location            = azurerm_resource_group.mynamegroup.location
-  os_type             = "p1v2"
-  sku_name            = "Windows"
+  name                = "${var.my_name}sp"
+  resource_group_name = azurerm_resource_group.seanrg.name
+  location            = azurerm_resource_group.seanrg.location
+  os_type             = "Windows"
+  sku_name            = "P1v2"
 }
 
 resource "azurerm_windows_web_app" "seanwebapp" {
-  name                = "${var.seanwebapp}wwa"
-  resource_group_name = azurerm_resource_group.mynamegroup.name
-  location            = azurerm_resource_group.mynamegroup.location
+  name                = "${var.my_name}wwa"
+  resource_group_name = azurerm_resource_group.seanrg.name
+  location            = azurerm_resource_group.seanrg.location
   service_plan_id     = azurerm_service_plan.seanserviceplan.id
 
   site_config {}
