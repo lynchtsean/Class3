64101242e0892631cd5d3a7dc1a7e7f6531c1c73
lynchtsean <lynchtsean@gmail.com>
2025-05-26 19:26:02 -0400

    Update level10.tf

--- level10.tf
+++ level10.tf
@@ -28,4 +28,40 @@ locals {
   }
 locals {
   apps = ["api", "frontend", "worker", "db"]
+  }
+locals {
+  cidrs = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  for_each = { for idx, cidr in local.cidrs : "subnet_${idx}" => cidr }
+
+  name                 = each.key
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = [each.value]
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+        "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"
+      ]
+    }
+  }
 }
