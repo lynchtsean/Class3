67a32fc855053000576f30d7085b95ec557715ae
lynchtsean <lynchtsean@gmail.com>
2025-05-26 18:38:45 -0400

    Update local.tf

--- local.tf
+++ local.tf
@@ -20,21 +20,3 @@ locals{
   original_text = "I AM A STUDENT AT MCIT"
   lower_text    = lower(local.original_text)
   }
-locals {
-  names = ["eastus", "westeurope", "centralindia"]
-  }
-locals {
-  vms = ["vm1", "vm2", "vm3"]
-
-  vms_map = { for idx, name in local.vms : "vm_${idx}" => name }
-  }
-locals {
-  rg_names = ["dev-rg", "test-rg", "prod-rg"]
-}
-
-resource "azurerm_resource_group" "rg" {
-  for_each = toset(local.rg_names)
-
-  name     = each.key
-  location = "East US"
-}
