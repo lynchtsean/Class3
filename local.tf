bde2636ccbfc4d4ee4727a5ebeea0a089395573b
lynchtsean <lynchtsean@gmail.com>
2025-05-26 18:33:35 -0400

    Update local.tf

--- local.tf
+++ local.tf
@@ -27,4 +27,14 @@ locals {
   vms = ["vm1", "vm2", "vm3"]
 
   vms_map = { for idx, name in local.vms : "vm_${idx}" => name }
+  }
+locals {
+  rg_names = ["dev-rg", "test-rg", "prod-rg"]
+}
+
+resource "azurerm_resource_group" "rg" {
+  for_each = toset(local.rg_names)
+
+  name     = each.key
+  location = "East US"
 }
