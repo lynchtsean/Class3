28268ed950c004ab81c8a9ddf97dce753776ff1f
lynchtsean <lynchtsean@gmail.com>
2025-05-26 18:44:52 -0400

    Update level10.tf

--- level10.tf
+++ level10.tf
@@ -8,11 +8,20 @@ locals {
   }
 locals {
   rg_names = ["dev-rg", "test-rg", "prod-rg"]
-}
+  }
 
 resource "azurerm_resource_group" "rg" {
   for_each = toset(local.rg_names)
 
   name     = each.key
   location = "East US"
+  }
+locals {
+  keys   = ["env", "team", "owner"]
+  values = ["prod", "devops", "alice"]
+
+  combined_map = {
+    for i in range(length(local.keys)) :
+    local.keys[i] => local.values[i]
+  }
 }
