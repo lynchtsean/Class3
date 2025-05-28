10a597b439f533dc918ecb6c1c74db0f1ce10e18
lynchtsean <lynchtsean@gmail.com>
2025-05-28 18:54:16 -0400

    Update subnetyaml.tf

--- subnetyaml.tf
+++ subnetyaml.tf
@@ -1,30 +1,25 @@
 locals {
-  location            = "eastus"
-  resource_group_name = "subnetsean-rg"
+  subnet_file = file("${path.module}/subnets.yaml")
 
-  subnet_map = {
-    frontend = "10.0.1.0/24"
-    backend  = "10.0.2.0/24"
-    db       = "10.0.3.0/24"
-  }
+  subnet_map = yamldecode(local.subnet_file).subnets
 }
 
-resource "azurerm_resource_group" "subnetsean-rg" {
-  name     = local.resource_group_name
-  location = local.location
+resource "azurerm_resource_group" "subnetsean_rg" {
+  name     = "subnetsean-rg"
+  location = "eastus"
 }
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "seanvnet-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = local.location
-  resource_group_name = local.resource_group_name
+  location            = azurerm_resource_group.subnetsean_rg.location
+  resource_group_name = azurerm_resource_group.subnetsean_rg.name
 }
 
 resource "azurerm_subnet" "subnets" {
   for_each             = local.subnet_map
   name                 = each.key
-  resource_group_name  = local.resource_group_name
+  resource_group_name  = azurerm_resource_group.subnetsean_rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = [each.value]
 }
