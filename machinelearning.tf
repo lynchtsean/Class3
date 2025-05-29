7b028bc368dd5a7c404a3735203afe046ab85f32
lynchtsean <lynchtsean@gmail.com>
2025-05-28 20:25:58 -0400

    Update machinelearning.tf

--- machinelearning.tf
+++ machinelearning.tf
@@ -1,11 +1,11 @@
 data "azurerm_client_config" "current" {}
 
-resource "azurerm_resource_group" "mlrg" {
+resource "azurerm_resource_group" "lynrg" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_application_insights" "applicationinsight" {
+resource "azurerm_application_insights" "lynapplicationinsight" {
   name                = var.app_insights_name
   location            = var.location
   resource_group_name = azurerm_resource_group.mlrg.name
@@ -15,7 +15,7 @@ resource "azurerm_application_insights" "applicationinsight" {
 resource "azurerm_key_vault" "azurekeyvault" {
   name                = var.key_vault_name
   location            = var.location
-  resource_group_name = azurerm_resource_group.mlrg.name
+  resource_group_name = azurerm_resource_group.lynrg.name
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "premium"
 }
@@ -23,7 +23,7 @@ resource "azurerm_key_vault" "azurekeyvault" {
 resource "azurerm_storage_account" "azurestorageaccount" {
   name                     = var.storage_account_name
   location                 = var.location
-  resource_group_name      = azurerm_resource_group.mlrg.name
+  resource_group_name      = azurerm_resource_group.lynrg.name
   account_tier             = "Standard"
   account_replication_type = "GRS"
 }
@@ -31,7 +31,7 @@ resource "azurerm_storage_account" "azurestorageaccount" {
 resource "azurerm_machine_learning_workspace" "azuremlworkspace" {
   name                    = var.ml_workspace_name
   location                = var.location
-  resource_group_name     = azurerm_resource_group.mlrg.name
+  resource_group_name     = azurerm_resource_group.lynrg.name
   application_insights_id = azurerm_application_insights.applicationinsight.id
   key_vault_id            = azurerm_key_vault.azurekeyvault.id
   storage_account_id      = azurerm_storage_account.azurestorageaccount.id
