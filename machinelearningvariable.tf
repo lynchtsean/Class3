e70ec76ca9c038480a554e9159cf323d6968a23b
lynchtsean <lynchtsean@gmail.com>
2025-05-28 20:11:38 -0400

    Update machinelearningvariable.tf

--- machinelearningvariable.tf
+++ machinelearningvariable.tf
@@ -1,1 +1,29 @@
+variable "location" {
+  description = "The Azure region to deploy resources in."
+  default     = "West Europe"
+}
 
+variable "resource_group_name" {
+  description = "Name of the resource group."
+  default     = "ml_rg-resources"
+}
+
+variable "app_insights_name" {
+  description = "Name of the Application Insights resource."
+  default     = "workspace-mcit-ai"
+}
+
+variable "key_vault_name" {
+  description = "Name of the Azure Key Vault."
+  default     = "workspaceexamplekeyvault"
+}
+
+variable "storage_account_name" {
+  description = "Name of the Azure Storage Account."
+  default     = "workspacestorageaccount"
+}
+
+variable "ml_workspace_name" {
+  description = "Name of the Azure Machine Learning Workspace."
+  default     = "mcit-workspace"
+}
