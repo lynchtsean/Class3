8f6ab60679fd75c9ab71214976832a8b90f70fcd
lynchtsean <lynchtsean@gmail.com>
2025-06-02 19:35:59 -0400

    Update output.tf

--- webapplication/firewall_policy/output.tf
+++ webapplication/firewall_policy/output.tf
@@ -1,1 +1,9 @@
+output "waf_policy_id" {
+  description = "ID of the WAF policy"
+  value       = azurerm_web_application_firewall_policy.example.id
+}
 
+output "waf_policy_name" {
+  description = "Name of the WAF policy"
+  value       = azurerm_web_application_firewall_policy.example.name
+}
