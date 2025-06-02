2f76274c29232bf49d288037c15ff2245817827d
lynchtsean <lynchtsean@gmail.com>
2025-06-02 19:19:33 -0400

    Update mcitstoragemodulecall.tf

--- mcitstoragemodulecall.tf
+++ mcitstoragemodulecall.tf
@@ -1,8 +1,8 @@
 module "mymcitstorage" {
   source              = "./modules/storage_account"
-  name                = "samistorageacc12345"
-  resource_group_name = azurerm_resource_group.mcit420zz5um.name
-  location            = azurerm_resource_group.mcit420zz5um.location
+  name                = "seanstorage514"
+  resource_group_name = azurerm_resource_group.mcitsean514.name
+  location            = azurerm_resource_group.mcitsean514.location
   account_tier        = "Standard"
   account_replication_type = "LRS"
   kind                = "StorageV2"
