fb91ea934eb6fa525cc78f5ab52bc024ce729616
lynchtsean <lynchtsean@gmail.com>
2025-06-02 19:00:23 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -60,15 +60,3 @@ variable "admin_password" {
   type        = string
   sensitive   = true
 }
-
-variable "location" {
-  description = "Azure region to deploy resources"
-  type        = string
-  default     = "West Europe"
-}
-
-variable "resource_group_name" {
-  description = "Name of the resource group"
-  type        = string
-  default     = "lynch-resources"
-}
