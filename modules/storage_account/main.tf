afa8f59afad104024155252d6aa18c81fc9093e1
lynchtsean <lynchtsean@gmail.com>
2025-06-02 19:04:36 -0400

    Update main.tf

--- modules/storage_account/main.tf
+++ modules/storage_account/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_storage_account" "mcitstoragemodule" {
   name                     = var.name
-  resource_group_name      = var.rm_group_name
-  location                 = var.place
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
   access_tier              = var.access_tier
