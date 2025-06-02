52793c85596c79accdc9bae3dd5f8652af4cff3e
lynchtsean <lynchtsean@gmail.com>
2025-06-02 19:24:40 -0400

    Update output.tf

--- modules/storage_account/output.tf
+++ modules/storage_account/output.tf
@@ -1,7 +1,7 @@
 output "storage_account_id" {
-  value = azurerm_storage_account.mcitstoragemodule.id
+  value = azurerm_storage_account.seanmcitstoragemodule.id
 }
 
 output "storage_account_name" {
-  value = azurerm_storage_account.mcitstoragemodule.name
+  value = azurerm_storage_account.seanmcitstoragemodule.name
 }
