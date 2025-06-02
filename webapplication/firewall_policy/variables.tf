078762044d84e6cd99534d70697f418db11ad73b
lynchtsean <lynchtsean@gmail.com>
2025-06-02 19:35:15 -0400

    Update variables.tf

--- webapplication/firewall_policy/variables.tf
+++ webapplication/firewall_policy/variables.tf
@@ -1,1 +1,17 @@
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+  default     = "example-rg"
+}
 
+variable "location" {
+  description = "Azure location"
+  type        = string
+  default     = "West Europe"
+}
+
+variable "waf_policy_name" {
+  description = "Name of the WAF policy"
+  type        = string
+  default     = "example-wafpolicy"
+}
