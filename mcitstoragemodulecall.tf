4de0e2ea9c3e97fa61910a4a0bd7851b9af40cf1
lynchtsean <lynchtsean@gmail.com>
2025-06-04 18:13:59 -0400

    Update mcitstoragemodulecall.tf

--- mcitstoragemodulecall.tf
+++ mcitstoragemodulecall.tf
@@ -1,65 +1,17 @@
+variable "storage_accounts" {
+  default = {
+    seanstorage514 = "mymcitstorage_1"
+    seanstorage515 = "mymcitstorage_2"
+    seanstorage516 = "mymcitstorage_3"
+    seanstorage517 = "mymcitstorage_4"
+    seanstorage518 = "mymcitstorage_5"
+  }
+}
+
 module "mymcitstorage" {
-  source              = "./modules/storage_account"
-  name                = "seanstorage514"
-  resource_group_name = azurerm_resource_group.lynchterraform.name
-  location            = azurerm_resource_group.lynchterraform.location
-  account_tier        = "Standard"
-  account_replication_type = "LRS"
-  kind                = "StorageV2"
-  access_tier         = "Hot"
-  tags = {
-    environment = "dev"
-    team        = "infra"
-  }
-}
-module "mymcitstorage_2" {
+  for_each                = var.storage_accounts
   source                  = "./modules/storage_account"
-  name                    = "seanstorage515"
-  resource_group_name     = azurerm_resource_group.lynchterraform.name
-  location                = azurerm_resource_group.lynchterraform.location
-  account_tier            = "Standard"
-  account_replication_type = "LRS"
-  kind                    = "StorageV2"
-  access_tier             = "Hot"
-  tags = {
-    environment = "dev"
-    team        = "infra"
-  }
-}
-
-module "mymcitstorage_3" {
-  source                  = "./modules/storage_account"
-  name                    = "seanstorage516"
-  resource_group_name     = azurerm_resource_group.lynchterraform.name
-  location                = azurerm_resource_group.lynchterraform.location
-  account_tier            = "Standard"
-  account_replication_type = "LRS"
-  kind                    = "StorageV2"
-  access_tier             = "Hot"
-  tags = {
-    environment = "dev"
-    team        = "infra"
-  }
-}
-
-module "mymcitstorage_4" {
-  source                  = "./modules/storage_account"
-  name                    = "seanstorage517"
-  resource_group_name     = azurerm_resource_group.lynchterraform.name
-  location                = azurerm_resource_group.lynchterraform.location
-  account_tier            = "Standard"
-  account_replication_type = "LRS"
-  kind                    = "StorageV2"
-  access_tier             = "Hot"
-  tags = {
-    environment = "dev"
-    team        = "infra"
-  }
-}
-
-module "mymcitstorage_5" {
-  source                  = "./modules/storage_account"
-  name                    = "seanstorage518"
+  name                    = each.key
   resource_group_name     = azurerm_resource_group.lynchterraform.name
   location                = azurerm_resource_group.lynchterraform.location
   account_tier            = "Standard"
