20a5f49b4be39254d9a9a69175714bf6e221ffba
lynchtsean <lynchtsean@gmail.com>
2025-06-04 19:15:06 -0400

    Update output.tf

--- modules/resource_group/output.tf
+++ modules/resource_group/output.tf
@@ -1,7 +1,7 @@
 output "name" {
-  value = azurerm_resource_group.this.name
+  value = azurerm_resource_group.snpp2025.name
 }
 
 output "location" {
-  value = azurerm_resource_group.this.location
+  value = azurerm_resource_group.snpp2025.location
 }
