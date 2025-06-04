dbdeeafa4700ba325da93a1505793722d66661ec
lynchtsean <lynchtsean@gmail.com>
2025-06-04 12:36:32 -0400

    Update local.tf

--- local.tf
+++ local.tf
@@ -23,7 +23,7 @@ locals{
 locals {
   region       = "eastus"
   is_primary   = local.region == "eastus" ? true : false
-  environment  = local.is_primary ? "production" : "staging"
+  environment_selection = local.is_primary ? "production" : "staging"
 }
  
 output "region" {
