e108b7d7f6d7a88d7df14d6d0f32e97b9ab65db8
lynchtsean <lynchtsean@gmail.com>
2025-06-04 12:27:36 -0400

    Update mcitstoragemodulecall.tf

--- mcitstoragemodulecall.tf
+++ mcitstoragemodulecall.tf
@@ -12,3 +12,62 @@ module "mymcitstorage" {
     team        = "infra"
   }
 }
+module "mymcitstorage_2" {
+  source                  = "./modules/storage_account"
+  name                    = "seanstorage515"
+  resource_group_name     = azurerm_resource_group.lynchterraform.name
+  location                = azurerm_resource_group.lynchterraform.location
+  account_tier            = "Standard"
+  account_replication_type = "LRS"
+  kind                    = "StorageV2"
+  access_tier             = "Hot"
+  tags = {
+    environment = "dev"
+    team        = "infra"
+  }
+}
+
+module "mymcitstorage_3" {
+  source                  = "./modules/storage_account"
+  name                    = "seanstorage516"
+  resource_group_name     = azurerm_resource_group.lynchterraform.name
+  location                = azurerm_resource_group.lynchterraform.location
+  account_tier            = "Standard"
+  account_replication_type = "LRS"
+  kind                    = "StorageV2"
+  access_tier             = "Hot"
+  tags = {
+    environment = "dev"
+    team        = "infra"
+  }
+}
+
+module "mymcitstorage_4" {
+  source                  = "./modules/storage_account"
+  name                    = "seanstorage517"
+  resource_group_name     = azurerm_resource_group.lynchterraform.name
+  location                = azurerm_resource_group.lynchterraform.location
+  account_tier            = "Standard"
+  account_replication_type = "LRS"
+  kind                    = "StorageV2"
+  access_tier             = "Hot"
+  tags = {
+    environment = "dev"
+    team        = "infra"
+  }
+}
+
+module "mymcitstorage_5" {
+  source                  = "./modules/storage_account"
+  name                    = "seanstorage518"
+  resource_group_name     = azurerm_resource_group.lynchterraform.name
+  location                = azurerm_resource_group.lynchterraform.location
+  account_tier            = "Standard"
+  account_replication_type = "LRS"
+  kind                    = "StorageV2"
+  access_tier             = "Hot"
+  tags = {
+    environment = "dev"
+    team        = "infra"
+  }
+}
