fb379d4a5c91152693c6b23e50dc8fa8cea62faa
lynchtsean <lynchtsean@gmail.com>
2025-06-04 19:16:58 -0400

    Create resourcegroupsnpp.tf

---
+++ resourcegroupsnpp.tf
@@ -0,0 +1,21 @@
+locals {
+  resource_groups = {
+    rg-dev-eastus   = "eastus"
+    rg-dev-westus   = "westus"
+    rg-dev-central  = "centralus"
+    rg-dev-canada   = "canadacentral"
+    rg-dev-uk       = "uksouth"
+  }
+}
+
+module "resource_groups" {
+  for_each = local.resource_groups
+
+  source   = "./modules/resource_group"
+  name     = each.key
+  location = each.value
+  tags = {
+    environment = "dev"
+    team        = "infra"
+  }
+}
