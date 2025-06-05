0ae4fa16d6a7778727e057f2e87ca513069462f5
lynchtsean <lynchtsean@gmail.com>
2025-06-05 08:37:20 -0400

    Update linuxwebappmodulecall.tf

--- linuxwebappmodulecall.tf
+++ linuxwebappmodulecall.tf
@@ -1,8 +1,8 @@
 module "linux_web_app" {
   source              = "./modules/linux_web_app"
   name                = "mymcitlinux-web-app"
-  location            = azurerm_resource_group.mcit420zz5um.location
-  resource_group_name = azurerm_resource_group.mcit420zz5um.name
+  location            = azurerm_resource_group.lynchterraform.location
+  resource_group_name = azurerm_resource_group.lynchterraform.name
 
   service_plan_name     = "mcit-app-service-plan"
   service_plan_sku_name = "B1"  # Example values: B1, S1, P1v3, EP1, etc.
