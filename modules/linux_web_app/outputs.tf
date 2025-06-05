4be964ce37c427715eb74f70d698df40b227aa77
lynchtsean <lynchtsean@gmail.com>
2025-06-05 08:36:17 -0400

    Update outputs.tf

--- modules/linux_web_app/outputs.tf
+++ modules/linux_web_app/outputs.tf
@@ -1,7 +1,7 @@
 output "web_app_id" {
-  value = azurerm_linux_web_app.this.id
+  value = azurerm_linux_web_app.lynchlinuxwebapp06052025.id
 }
 
 output "default_hostname" {
-  value = azurerm_linux_web_app.this.default_hostname
+  value = azurerm_linux_web_app.lynchlinuxwebapp06052025.default_hostname
 }
